65bd438eb3ab87d949feb2f6a59f4d8a70d06cfa
mostafa <me.cssign@gmail.com>
2021-07-09 23:41:35 +0200

    Initialize a Spring Boot project

---
+++ src/test/java/mostafaism/com/github/fibonaccinumbersgame/FibonacciNumbersGameApplicationTests.java
@@ -0,0 +1,13 @@
+package mostafaism.com.github.fibonaccinumbersgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibonacciNumbersGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
