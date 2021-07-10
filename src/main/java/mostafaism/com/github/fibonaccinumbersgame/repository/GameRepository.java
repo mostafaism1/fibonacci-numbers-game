fef98be3cb2411c9da7415fc465720b805cdd5f4
mostafa <me.cssign@gmail.com>
2021-07-10 16:52:06 +0200

    Create a Game repository

---
+++ src/main/java/mostafaism/com/github/fibonaccinumbersgame/repository/GameRepository.java
@@ -0,0 +1,11 @@
+package mostafaism.com.github.fibonaccinumbersgame.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import mostafaism.com.github.fibonaccinumbersgame.model.entity.Game;
+
+@Repository
+public interface GameRepository extends CrudRepository<Game, Long> {
+
+}
