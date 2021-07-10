4df0964801c890d2978f1967db863747796aa074
mostafa <me.cssign@gmail.com>
2021-07-11 00:46:24 +0200

    Define GameService.getOnTurnPlayer() service method

--- src/main/java/mostafaism/com/github/fibonaccinumbersgame/service/GameService.java
+++ src/main/java/mostafaism/com/github/fibonaccinumbersgame/service/GameService.java
@@ -3,6 +3,7 @@ package mostafaism.com.github.fibonaccinumbersgame.service;
 import java.util.UUID;
 
 import mostafaism.com.github.fibonaccinumbersgame.model.entity.Game;
+import mostafaism.com.github.fibonaccinumbersgame.model.entity.Player;
 import mostafaism.com.github.fibonaccinumbersgame.model.request.CreateGameRequest;
 
 public interface GameService {
@@ -10,5 +11,7 @@ public interface GameService {
     Game createGame(CreateGameRequest createGameRequest);
 
     boolean deleteGame(UUID gameCode);
+
+    Player getOnTurnPlayer(UUID gameCode);
 
 }
