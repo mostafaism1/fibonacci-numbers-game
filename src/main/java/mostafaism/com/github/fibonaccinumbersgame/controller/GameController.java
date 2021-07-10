7a333fd1659486fc3bdea41f32c1a01d4c48ff8a
mostafa <me.cssign@gmail.com>
2021-07-11 00:47:50 +0200

    Add an endpoint to get the on-turn player for a given game

--- src/main/java/mostafaism/com/github/fibonaccinumbersgame/controller/GameController.java
+++ src/main/java/mostafaism/com/github/fibonaccinumbersgame/controller/GameController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +15,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
+import mostafaism.com.github.fibonaccinumbersgame.model.entity.Game;
+import mostafaism.com.github.fibonaccinumbersgame.model.entity.Player;
 import mostafaism.com.github.fibonaccinumbersgame.model.mapper.GameToGameResponseMapper;
+import mostafaism.com.github.fibonaccinumbersgame.model.mapper.PlayerToNextPlayerResponseMapper;
 import mostafaism.com.github.fibonaccinumbersgame.model.request.CreateGameRequest;
 import mostafaism.com.github.fibonaccinumbersgame.model.response.GameResponse;
+import mostafaism.com.github.fibonaccinumbersgame.model.response.NextPlayerResponse;
 import mostafaism.com.github.fibonaccinumbersgame.service.GameService;
 
 @RestController
@@ -25,18 +30,27 @@ import mostafaism.com.github.fibonaccinumbersgame.service.GameService;
 public class GameController {
 
     private final GameService gameService;
-    private final GameToGameResponseMapper mapper;
+    private final GameToGameResponseMapper gameToGameResponseMapper;
+    private final PlayerToNextPlayerResponseMapper playerToNextPlayerResponseMapper;
 
     @PostMapping(path = "/new-game", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(code = HttpStatus.CREATED)
     public GameResponse createGame(@RequestBody @Valid CreateGameRequest createGameRequest) {
-        return mapper.apply(gameService.createGame(createGameRequest));
+        Game game = gameService.createGame(createGameRequest);
+        return gameToGameResponseMapper.apply(game);
     }
 
     @PostMapping(path = "/{gameCode}/end")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void endGame(@PathVariable(name = "gameCode") UUID gameCode) {
         gameService.deleteGame(gameCode);
+    }
+
+    @GetMapping(path = "/{gameCode}/turn")
+    @ResponseStatus(code = HttpStatus.OK)
+    public NextPlayerResponse getNextPlayer(@PathVariable(name = "gameCode") UUID gameCode) {
+        Player onTurnPlayer = gameService.getOnTurnPlayer(gameCode);
+        return playerToNextPlayerResponseMapper.apply(onTurnPlayer);
     }
 
 }
