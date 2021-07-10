8b619d2457b3cda7252aff998e0c6255991f7353
mostafa <me.cssign@gmail.com>
2021-07-10 19:23:28 +0200

    Add GameRepository.deleteByGameCode() method

--- src/main/java/mostafaism/com/github/fibonaccinumbersgame/repository/GameRepository.java
+++ src/main/java/mostafaism/com/github/fibonaccinumbersgame/repository/GameRepository.java
@@ -1,5 +1,7 @@
 package mostafaism.com.github.fibonaccinumbersgame.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,7 @@ import mostafaism.com.github.fibonaccinumbersgame.model.entity.Game;
 public interface GameRepository extends CrudRepository<Game, Long> {
 
     int countByIsEnded(boolean isEnded);
+
+    void deleteByGameCode(UUID gameCode);
 
 }
