97468661ed0916768bda06cd38eb1bff95d2a743
mostafa <me.cssign@gmail.com>
2021-07-10 18:37:02 +0200

    Validate player names are unique

--- src/main/java/mostafaism/com/github/fibonaccinumbersgame/model/request/CreateGameRequest.java
+++ src/main/java/mostafaism/com/github/fibonaccinumbersgame/model/request/CreateGameRequest.java
@@ -6,10 +6,13 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.UniqueElements;
+
 import lombok.Data;
 
 @Data
 public class CreateGameRequest {
     @Size(min = 2, max = 10)
+    @UniqueElements
     List<@NotBlank @Size(max = 8, message = "Player name must be less than 9 characters long") @Pattern(regexp = "^[A-Za-z0-9]*$") String> players;
 }
