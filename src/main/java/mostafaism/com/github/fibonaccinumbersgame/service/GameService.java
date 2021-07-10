f65317db3c2910edcaf02f033c9f7779301841f2
mostafa <me.cssign@gmail.com>
2021-07-10 19:22:52 +0200

    Define  GameService.deleteGame() service method

--- src/main/java/mostafaism/com/github/fibonaccinumbersgame/service/GameService.java
+++ src/main/java/mostafaism/com/github/fibonaccinumbersgame/service/GameService.java
@@ -1,10 +1,14 @@
 package mostafaism.com.github.fibonaccinumbersgame.service;
 
+import java.util.UUID;
+
 import mostafaism.com.github.fibonaccinumbersgame.model.entity.Game;
 import mostafaism.com.github.fibonaccinumbersgame.model.request.CreateGameRequest;
 
 public interface GameService {
 
     Game createGame(CreateGameRequest createGameRequest);
+
+    boolean deleteGame(UUID gameCode);
 
 }
