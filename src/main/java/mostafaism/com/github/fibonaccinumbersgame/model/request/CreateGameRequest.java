f0d2e8ebf3fa8c9f5d98b0f5f3892fea6215d00e
mostafa <me.cssign@gmail.com>
2021-07-10 17:58:05 +0200

    Add validations to CreateGameRequest players field

--- src/main/java/mostafaism/com/github/fibonaccinumbersgame/model/request/CreateGameRequest.java
+++ src/main/java/mostafaism/com/github/fibonaccinumbersgame/model/request/CreateGameRequest.java
@@ -2,12 +2,14 @@ package mostafaism.com.github.fibonaccinumbersgame.model.request;
 
 import java.util.List;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
 @Data
 public class CreateGameRequest {
-    @NotEmpty
-    List<String> players;
+    @Size(min = 2, max = 10)
+    List<@NotBlank @Size(max = 8, message = "Player name must be less than 9 characters long") @Pattern(regexp = "^[A-Za-z0-9]*$") String> players;
 }
