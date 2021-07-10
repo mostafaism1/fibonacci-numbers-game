7151f0cccee94b9db56b0557758b86e841db8216
mostafa <me.cssign@gmail.com>
2021-07-10 02:12:08 +0200

    Define a GamePlayer entity

---
+++ src/main/java/mostafaism/com/github/fibonaccinumbersgame/model/entity/GamePlayer.java
@@ -0,0 +1,43 @@
+package mostafaism.com.github.fibonaccinumbersgame.model.entity;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Table(name = "game_player")
+@Data
+@AllArgsConstructor
+@Builder
+public class GamePlayer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "player_id")
+    private Player player;
+
+    @Column(name = "player_code")
+    private UUID playerCode;
+
+}
