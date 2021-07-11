882d479ac832efa2feaba9b734da13071ca82833
mostafa <me.cssign@gmail.com>
2021-07-11 12:35:16 +0200

    Create a PlayerScoreResponse class

---
+++ src/main/java/mostafaism/com/github/fibonaccinumbersgame/model/response/PlayerScoreResponse.java
@@ -0,0 +1,14 @@
+package mostafaism.com.github.fibonaccinumbersgame.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class PlayerScoreResponse {
+
+    private int playerScore;
+
+}
