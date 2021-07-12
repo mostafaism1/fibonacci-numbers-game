550ff5d5a0f6312adea3501be144f6761053b555
mostafa <me.cssign@gmail.com>
2021-07-12 03:53:29 +0200

    Add a ResponseStatus to MaximumGamesLimitReachedException

--- src/main/java/mostafaism/com/github/fibonaccinumbersgame/exception/MaximumGamesLimitReachedException.java
+++ src/main/java/mostafaism/com/github/fibonaccinumbersgame/exception/MaximumGamesLimitReachedException.java
@@ -1,5 +1,9 @@
 package mostafaism.com.github.fibonaccinumbersgame.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "The maximum number of active games has been reached. Please try again later")
 public class MaximumGamesLimitReachedException extends RuntimeException {
 
     public MaximumGamesLimitReachedException() {
